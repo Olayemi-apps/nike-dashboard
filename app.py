cc328e257eadac54d767a32406e574b6c5c5407f
Olayemi-apps <51457442+Olayemi-apps@users.noreply.github.com>
2022-11-03 15:04:47 +0000

    server position update

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ external_stylesheets = [
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
                 meta_tags=[{"name": "viewport", "content": "width=device-width"}])
+server = app.server
 
 
 app.layout = html.Div([
